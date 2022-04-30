685915e213ee921dfef7a33a681886eb37fc6d25
Gennady Yonov <Gennady_Yonov@swissre.com>
2022-04-30 09:01:55 +0300

    -- spring initializr

---
+++ src/main/java/lv/gennadyyonov/spring/jpa/multiple/db/SpringJpaMultipleDbDemoApplication.java
@@ -0,0 +1,13 @@
+package lv.gennadyyonov.spring.jpa.multiple.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaMultipleDbDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaMultipleDbDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lv/gennadyyonov/spring/jpa/multiple/db/SpringJpaMultipleDbDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package lv.gennadyyonov.spring.jpa.multiple.db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaMultipleDbDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
